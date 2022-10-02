92675b3e4692dd3aa717bbd3b7c7e1a99ac6cd80
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2022-10-02 18:44:27 +0300

    some refinements

--- console_bot/app/bot.py
+++ console_bot/app/bot.py
@@ -5,81 +5,72 @@ EXIT_WORDS = ["exit", "close", "good bye", "end"]
 
 def input_error(func):
     def wrapper(*args, **kwargs):
-        while True:
-            try:
-                func(*args, **kwargs)
-                break
-            except Exception:
-                print("Incorrect input data.")
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "Wrong name."
+        except TypeError:
+            return "Wrong command format."
+        except IndexError:
+            return "Wrong input data. Enter name and phone."
+        except ValueError as e:
+            return e.args[0]
+        except Exception as e:
+            return e.args
     return wrapper
 
 
 @input_error
-def add_contact():
-
-    name, number = input("Enter name and number: ").split()
-    if name in contacts:
-        print("The contact with name '{name}' is already exists.")
-    else:
-        contacts[name] = number
-        print(f"Contact {name} added.")
-
-
-@input_error
-def find_contact():
-
-    name = input("Enter name: ")
-
-    if name in contacts:
-        print(f"{name}'s number: {contacts[name]}")
-    else:
-        print(f"Contact with name '{name}' doesn't exists.")
+def add_contact(name, number=None):
+    """Fucn which adds new contact in contacts"""
+    if number is None:
+        raise IndexError
+    elif name in contacts:
+        raise ValueError(f"{name} is already in contacts")
+    contacts[name] = number
+    return f"Contact with name {name} and phone {number} was added."
 
 
 @input_error
-def change_contact():
-
-    name, number = input("Enter name and number: ").split()
-    if name in contacts:
-        print(
-            f"The {name}'s number will be changed from {contacts[name]} to {number}")
-        contacts[name] = number
-    else:
-        print(f"Contact with name '{name}' doesn't exists.")
+def find_contact(name, number=None):
+    """Fucn which shows name's number"""
+    return contacts[name]
 
 
 @input_error
-def show_all_contacts():
-    print()
+def change_contact(name, new_number=None):
+    """Fucn which changes some contact"""
+    if name in contacts:
+        contacts[name] = new_number
+        return f"Contact's number with name {name} was changed to {new_number}."
+    else:
+        raise ValueError("Unknown name.")
+
+
+@input_error
+def show_all_contacts(*args):
+    """Fucn which shows all contacts"""
     if contacts:
-        a = len(max(contacts.keys(), key=len))
-        b = len(max(contacts.values(), key=len))
-        print("All contacts:")
-        print((a + b + 6) * "-")
-        [print(f"{key} --->  {value}") for key, value in contacts.items()]
-        print((a + b + 6) * "-")
+        return '\n'.join([f"{key} ---:  {value}" for key, value in contacts.items()])
     else:
-        print("You don't have any contacts.")
+        return "You don't have any contacts."
 
 
 @input_error
-def remove_contact():
-
-    name = input("Enter name: ")
-    if name in contacts:
-        del contacts[name]
-        print(f"Contact {name} was removed.")
-    else:
-        print(f"Contact with name '{name}' doesn't exists.")
+def remove_contact(name, number=None):
+    """Fucn which deletes contact"""
+    del contacts[name]
+    return f"Contact with name {name} was removed."
 
 
 @input_error
-def show_all_commands():
-    print(*handler.keys(), sep=" # ")
+def show_all_commands(name=None, number=None):
+    """Fucn which shows all commands"""
+    return " # ".join(handler.keys())
 
 
 handler = {
-    "hello": lambda: print("Hello! How can I help you?"),
+    "hello": lambda: "Hello! How can I help you?",
     "add": add_contact,
     "change": change_contact,
     "phone": find_contact,
@@ -91,13 +82,15 @@ handler = {
 
 def main():
     while True:
-        command = input("Enter command: ").lower().strip()
-        if command in EXIT_WORDS:
+
+        command = input("Enter command: ").lower().strip().split()
+
+        if command[0] in EXIT_WORDS:
             print("See ya!")
             exit()
 
-        elif command in handler:
-            handler[command]()
+        elif command[0] in handler:
+            print(handler[command[0]](*command[1:]))
         else:
             print("Incorrect command. (To see all commands enter 'commands')")
 
